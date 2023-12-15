35dd817a0a41256e0fc5301b6f30d488c86c7f9a
Paweł Musielak <pmusielak@edu.cdv.pl>
2023-12-15 11:27:28 +0100

    lab3 - database connection works

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -4,13 +4,14 @@ using Microsoft.Extensions.DependencyInjection;
 using CdvAzure.Functions;
 using Microsoft.EntityFrameworkCore;
 using lab3.Database;
+using lab3.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddSingleton<PeopleService>();
+        services.AddSingleton<DatabasePeopleService>();
         services.AddDbContext<PeopleDb>(options => {
             options.UseSqlServer("Server=tcp:iot-2023.database.windows.net,1433;Initial Catalog=IoT_2023;Persist Security Info=False;User ID=PMusielak;Password=Wrotki12#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         });
